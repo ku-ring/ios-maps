a25674780890ebe97e94714e69fd733756001cd1
Jung Hwan Park <hwann1005@gmail.com>
2026-08-19 18:34:45 +0900

    Nit remove scrollindicator

--- Sources/KuringMapsUI/Map/BottomSheet/CategoryPlaceListView.swift
+++ Sources/KuringMapsUI/Map/BottomSheet/CategoryPlaceListView.swift
@@ -40,6 +40,7 @@ struct CategoryPlaceListView: View {
                     .padding(.top, 32)
                     .padding(.bottom, 24)
                 }
+                .scrollIndicators(.never)
             }
         }
         .background(appearance.bg)
